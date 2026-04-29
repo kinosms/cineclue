7847338868aabce1502ec717a732323042753f72
kinosms <kinosms@kinosmsui-iMac.local>
2026-04-29 21:13:18 +0900

    feat: objective lock UX + scroll fix

--- app/CineClue.js
+++ app/CineClue.js
@@ -2054,6 +2054,7 @@ style={{
           <button
             key={i}
             onClick={()=>{
+              if(selectedChoice === c) return
               setSelectedChoice(c)
               submit(c)
             }}
