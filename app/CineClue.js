4bb2835f4a69f45affc700b530df1866eb54b1d7
kinosms <kinosms@kinosmsui-iMac.local>
2026-05-10 14:44:18 +0900

    fix ipad youtube overlay issue

--- app/CineClue.js
+++ app/CineClue.js
@@ -6651,7 +6651,6 @@ export default function CineClue()  {
                 title="YouTube player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
               />
           </div>
         </div>
