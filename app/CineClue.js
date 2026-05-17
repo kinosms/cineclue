087f1aefaed787aae35524d638195949744852d7
kinosms <kinosms@kinosmsui-iMac.local>
2026-05-17 21:43:32 +0900

    console arrange

--- app/CineClue.js
+++ app/CineClue.js
@@ -659,7 +659,7 @@ async function safeQuery(promise, label = 'query') {
     const result = await withTimeout(
       promise,
       label,
-      10000
+      5000
     )
 
     if (result.error) {
