87e1a9d1a5b8ce172c765e4ea41cfd9ebf41c13f
kinosms <kinosms@kinosmsui-iMac.local>
2026-06-07 15:05:24 +0900

    add google search console verification

---
+++ app/sitemap.js
@@ -0,0 +1,19 @@
+export default function sitemap() {
+
+  return [
+
+    {
+
+      url: 'https://cine-clue.com',
+
+      lastModified: new Date(),
+
+      changeFrequency: 'daily',
+
+      priority: 1,
+
+    },
+
+  ]
+
+}
